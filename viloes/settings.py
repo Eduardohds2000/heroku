72386a750c7ff8940a9b460990fda27530412b50
eduardo <eduardodinuz23@gmail.com>
2019-07-04 15:59:39 -0300

    heruko

--- viloes/settings.py
+++ viloes/settings.py
@@ -10,7 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
-import os
+import o
+import django_heruko
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -122,3 +123,5 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, MEDIA_URL)
+
+django_heruko.settings(locals())
